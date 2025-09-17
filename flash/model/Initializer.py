63753145327ae2f3f07cac7044fa7b77fde5f3a6
zengxiangquan <zengxiangquan@staff.deepexi.com>
2025-09-17 10:20:02 +0800

    zxq-[add readme.md]

--- flash/model/Initializer.py
+++ flash/model/Initializer.py
@@ -2,9 +2,13 @@
 # @Author : ZXQ
 # @Time : 2025/9/10 18:53
 import os
+import sys
+import json
 
 import geemap
 import ee
+
+from flash.model.DataSourceConfigure import DataSourceConfigure
 
 
 class Initializer:
@@ -16,22 +20,24 @@ class Initializer:
         os.environ['HTTPS_PROXY'] = "http://127.0.0.1:7897"
         pass
 
-    def initialize(self):
+    def initialize(self, data_source_configure: DataSourceConfigure):
         '''
         初始化
         :return:
         '''
 
-        # 服务账号邮箱（在 JSON 文件里能看到）
-        service_account = "token-324@corn-427712.iam.gserviceaccount.com"
+
         # JSON 密钥文件路径
-        key_file = r"E:\pysiderLearn\pysiderProject\flash\key\corn-427712-6bb826b25e07.json"
+        key_file = os.path.join(data_source_configure.data_path_config.base_path,'key')
+        files = os.listdir(key_file)[0]
+        key_file = os.path.join(key_file,files)
+        with open(key_file, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        service_account = data["client_email"]
 
         # 用服务账号凭证初始化 Earth Engine
         credentials = ee.ServiceAccountCredentials(service_account, key_file)
         ee.Initialize(credentials)
-
-
         pass
 
 if __name__ == '__main__':
